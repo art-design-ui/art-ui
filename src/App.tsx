67396854b36cafd5fbfd2b7e2f454670dd157fae
wanggangfeng <wanggangfeng@forchange.tech>
2020-05-31 18:13:15 +0800

    feat: 🎸 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/index.scss";
 
 function App() {
   return (
