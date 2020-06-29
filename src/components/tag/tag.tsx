60f8070535c52b56f02ff0684efe2084bbba8d46
liangyu <liangyu@wead.mobi>
2020-06-29 14:12:16 +0800

    style: 修改样式文件

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -1,41 +1,29 @@
-import React, { FC, HTMLAttributes, useRef, useEffect } from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import classNames from 'classnames'
+import { getPrefixCls } from '@util'
 
 export type TagSize = 'lg' | 'sm' | 'default'
 export type TagType = 'primary' | 'default' | 'danger' | 'warning' | 'success' | 'info'
 
 interface BaseTagProps {
-  className?: string
   /** 设置标签大小 */
   size?: TagSize
   /** 设置标签类型 */
   type?: TagType
-  /** 回调ref，组件加载完成回调ref，返回值会在卸载组件时调用 */
-  refcallback?: (ref: any) => any
+  className?: string
 }
 
 export type TagProps = HTMLAttributes<HTMLSpanElement> & BaseTagProps
 
+const prefixCls = getPrefixCls('tag')
 export const Tag: FC<TagProps> = (props: TagProps) => {
-  const { size, type, children, refcallback, className, ...restProps } = props
-  const classes = classNames('art-tag', className, {
-    [`art-tag-type-${type}`]: type,
-    [`art-tag-size-${size}`]: size,
+  const { size, type, children, className, ...restProps } = props
+  const classes = classNames(prefixCls, className, {
+    [`${prefixCls}-${type}`]: type,
+    [`${prefixCls}-${size}`]: size,
   })
-  const tagRef = useRef(null)
-  useEffect(() => {
-    let uninstall: any = null
-    if (refcallback) {
-      uninstall = refcallback(tagRef)
-    }
-    return () => {
-      if (typeof uninstall === 'function') {
-        uninstall()
-      }
-    }
-  }, [refcallback])
   return (
-    <span className={classes} {...restProps} ref={tagRef}>
+    <span className={classes} {...restProps}>
       {children}
     </span>
   )
